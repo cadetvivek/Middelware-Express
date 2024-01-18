aab61b5c3ed2c499b10b8c34c5a7263ec4406dbd
cadetvivek <kushwahvivek805@gmail.com>
2024-01-18 11:44:37 +0530

    use middelware in express

---
+++ script.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+//app.set('view engine','ejs')
+
+app.use(function(req, res,next)  {
+    console.log('hello form middeware')
+    next();
+  })
+
+  app.use(function(req,res,next){
+    console.log("hello form middleware 2")
+  })
+
+app.get('/', function(req, res)  {
+  res.send('hellow vivek kushwah')
+})
+
+app.listen(3000);
